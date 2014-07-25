fbdfa0c63f3dfbad8becbd51bdd89d13e22aea5f
muratayusuke <info@muratayusuke.com>
2014-07-24 22:16:45 -0700

    add fucntion to get properties of NSObject

---
+++ Categories/NSObject+Util.h
@@ -0,0 +1,16 @@
+//
+//  NSObject+Util.h
+//  Best10
+//
+//  Created by Yusuke Murata on 2014/07/25.
+//  Copyright (c) 2014 Yusuke Murata (http://www.muratayusuke.com/)
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (Util)
+
+- (NSArray *)propertyKeys;
+- (NSDictionary *)properties;
+
+@end
