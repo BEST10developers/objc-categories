7a132287761ffeee289c78db61f20bb3592b9ea8
muratayusuke <info@muratayusuke.com>
2014-12-19 17:10:33 +0900

    add function to show debug log of NSDictionary including Japanese

---
+++ Categories/NSDictionary+Debug.h
@@ -0,0 +1,15 @@
+//
+//  NSDictionary+Debug.h
+//  Best10
+//
+//  Created by 村田 佑介 on 2014/12/19.
+//  Copyright (c) 2014年 Best10, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (Debug)
+
+- (NSString *)BT_descriptionWithEncoding;
+
+@end
